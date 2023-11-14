2cf78fdfc6336668e067f5e9e16265c731188914
PhamQuocTien <131446472+LateNight229@users.noreply.github.com>
2023-11-14 21:40:41 +0700

    init project

--- Assets/Scripts/Stats/BattleStat/CoinPool.cs
+++ Assets/Scripts/Stats/BattleStat/CoinPool.cs
@@ -4,31 +4,48 @@ using UnityEngine;
 
 public class CoinPool : MonoBehaviour
 {
-    public GameObject coinPrefabs;
-    public int coinCount;
-    public List<GameObject> coinList = new List<GameObject>();
+    public GameObject coin;
+    public int countCoinInitial;
+    public List<GameObject> listCoin = new List<GameObject>();
     void Start()
     {
-        for(int i = 0; i < coinCount; i++)
-        {
-            GameObject coin = Instantiate(coinPrefabs);
-            coin.SetActive(false);
-            coinList.Add(coin);
-        }
-        GameObject coin1 = GetCoin();
-        coin1.transform.position = new Vector2(0, 2);
+        CreateCoinPool();
     }
-
-    public GameObject GetCoin()
+    void CreateCoinPool()
     {
-        for(int i = 0;i < coinList.Count; i++)
+        if (listCoin == null || listCoin.Count == 0)
         {
-            if (!coinList[i].activeInHierarchy)
+            listCoin = new List<GameObject>();
+
+            for (int i = 0; i < countCoinInitial; i++)
             {
-                coinList[i].SetActive(true);
-                return coinList[i];
+                var _coin = Instantiate(coin, Vector3.zero, Quaternion.identity);
+                _coin.SetActive(false);
+                listCoin.Add(_coin);
             }
+
+            SplitOutCoin();
+        }
+    }
+    public GameObject AppearCoin()
+    {
+        for (int i = 0; i < listCoin.Count; i++)
+        {
+            if (listCoin[i] != null && !listCoin[i].activeInHierarchy)
+            {
+                return listCoin[i];
+            }
+
         }
         return null;
+    }
+    public void SplitOutCoin()
+    {
+        for (int i = 0; i < countCoinInitial; i++)
+        {
+            var obj = AppearCoin();
+            obj.transform.position = new Vector2(Random.Range(6, 10), Random.Range(0, 6));
+            obj.SetActive(true);
+        }
     }
 }
